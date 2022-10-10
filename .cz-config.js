c2826ce84b78fafe21e0d62860f48f50bd187bcb
alan.xiao <alan.xiao@d1m.cn>
2022-10-10 22:15:59 +0800

    stash(config):

--- .cz-config.js
+++ .cz-config.js
@@ -98,9 +98,6 @@ module.exports = {
     footer: '列举出所有变更的 ISSUES CLOSED（可选）。 例如: #31, #34：\n',
     confirmCommit: '确认提交？'
   },
-  allowCustomScopes: true,
   allowBreakingChanges: ['feat', 'fix'],
-  subjectLimit: 100,
-
   skipQuestions: ['body', 'footer']
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import './index.less'
 import Todo from './Todo/index'
 import AntdTable from './AntdTable/index'
-
 const Component: React.FunctionComponent = (): JSX.Element => {
   return (
     <div className="hello">
